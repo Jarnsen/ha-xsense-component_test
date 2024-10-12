22d5d73d1c19307dcab1b4d826d24de157fb97a4
Jarnsen <jarno.karsch@vodafone.de>
2024-10-12 18:36:40 +0200

    Update __init__.py

--- custom_components/xsense/__init__.py
+++ custom_components/xsense/__init__.py
@@ -1,4 +1,5 @@
 """The X-Sense Home Security integration."""
+
 from __future__ import annotations
 
 from homeassistant.config_entries import ConfigEntry
@@ -8,7 +9,7 @@ from homeassistant.core import HomeAssistant
 from .const import DOMAIN
 from .coordinator import XSenseDataUpdateCoordinator
 
-PLATFORMS: list[Platform] = [Platform.SENSOR, Platform.BINARY_SENSOR]
+PLATFORMS: list[Platform] = [Platform.BINARY_SENSOR, Platform.BUTTON, Platform.SENSOR]
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
