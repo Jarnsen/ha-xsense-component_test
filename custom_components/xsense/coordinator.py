008ebf0a8efd23883824a2c1e1a914feac522e11
Jarnsen <jarno.karsch@vodafone.de>
2025-01-18 13:28:57 +0100

    Update coordinator.py
    update

--- custom_components/xsense/coordinator.py
+++ custom_components/xsense/coordinator.py
@@ -221,7 +221,7 @@ class XSenseDataUpdateCoordinator(DataUpdateCoordinator[dict[str, Any]]):
         """Ask MQTT server for updates for all realtime devices, such as temperature sensor."""
         for s in house.stations.values():
             updatable_devices = [
-                dev.sn for dev in s.devices.values() if dev.type in ["STH51"]
+                dev.sn for dev in s.devices.values() if dev.type in ["STH51", "STH0A"]
             ]
 
             if not updatable_devices:
