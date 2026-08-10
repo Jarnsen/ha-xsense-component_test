d56aaed95098e2797f7673360fde56caf1d97acc
Wheemer <wheemer@hotmail.com>
2026-08-10 09:29:27 -0230

    Remove unused Pion adapter cleanup

--- custom_components/xsense/recordings_media.py
+++ custom_components/xsense/recordings_media.py
@@ -3,7 +3,6 @@
 from __future__ import annotations
 
 import asyncio
-from contextlib import suppress
 from datetime import datetime, timedelta, timezone
 from pathlib import Path
 from time import monotonic
